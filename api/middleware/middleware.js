46459568042cebc81bf1ac69a6235acb9b814aaf
BunniMK <86545296+BunniMK@users.noreply.github.com>
2022-01-12 13:57:31 -0700

    Making validations

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -5,16 +5,30 @@ function logger(req, res, next) {
   // DO YOUR MAGIC
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+async function validateUserId(req, res, next) {
+  const userId = await Users.findById(req.params.id)
+  if(userId){
+    req.user = userId
+    next()
+  } else {
+    next({ status: 404, message: "user not found" })
+  }
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  if(!req.body.name){
+    next({ status: 400, message: "missing required name field" })
+  } else {
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  if(!req.body.text){
+    next({ status: 400, message: "missing required text field" })
+  } else {
+    next()
+  }
 }
 
 // do not forget to expose these functions to other modules
